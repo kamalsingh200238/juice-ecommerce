2ea99e19d79f5a6447f866204be65b140d30b485
Kamal Singh <kamalsingh200238@gmail.com>
2023-03-18 21:04:09 +0530

    feat: created product schema

---
+++ schemas/product.ts
@@ -0,0 +1,22 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "product",
+  title: "Products",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+      },
+    }),
+  ],
+});
