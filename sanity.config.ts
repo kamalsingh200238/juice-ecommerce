7c9aea19912c22bab30f373092945e235fd94926
Kamal Singh <kamalsingh200238@gmail.com>
2023-03-18 21:04:39 +0530

    feat: config for sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,19 @@
+import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
+import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
 import { deskTool } from "sanity/desk";
+import product from "./schemas/product";
 
 export default defineConfig({
   basePath: "/admin",
   projectId: "tf7pn90e",
   dataset: "production",
   title: "Juice Ecommerce",
-  plugins: [deskTool()]
+  plugins: [deskTool(),
+    unsplashImageAsset(),
+    visionTool(),
+  ],
+  schema: {
+    types: [product],
+  },
 });
-
