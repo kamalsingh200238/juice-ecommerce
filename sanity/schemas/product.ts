94842b8b34fcb96eb5c1c3b519633fbc523d4550
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-30 13:39:26 +0530

    feat(sanity/schemas/product.ts): Add schema for products
    - name
    - slug
    - description
    - price
    - array of images with caption

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,52 @@
+import { defineArrayMember, defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'product',
+  type: 'document',
+  title: 'Products',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            defineField({
+              name: 'caption',
+              title: 'Caption',
+              type: 'string',
+            }),
+          ],
+        }),
+      ],
+    }),
+  ],
+});
