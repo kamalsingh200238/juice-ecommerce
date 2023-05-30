c5595c5a7cf162a4fa1d67eb00453f6112af4ba9
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-30 14:01:39 +0530

    feat(frontend/santiy/client.ts): Create client to connect sanity with frontend

---
+++ frontend/sanity/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from "next-sanity";
+
+const client = createClient({
+  projectId: "yzmnvt3b",
+  dataset: "production",
+  apiVersion: "2023-05-29",
+  useCdn: false,
+});
+
+export default client;
