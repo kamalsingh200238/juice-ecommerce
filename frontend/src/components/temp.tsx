40d10750bb62063f661129e9d2336396f7ee72b5
Kamal Singh <kamalsingh200238@gmail.com>
2023-07-16 11:56:45 +0530

    feat(frontend): Display the quantity of product present in the cart - On everyproduct a button shows if that product is present in the cart

--- frontend/src/components/temp.tsx
+++ frontend/src/components/temp.tsx
@@ -8,22 +8,28 @@ export default function Temp({ products }: { products: Product[] }) {
   const cart = useCartStore((state) => state.cart);
   const increaseQuantity = useCartStore((state) => state.increaseQuantity);
   const decreaseQuantity = useCartStore((state) => state.decreaseQuantity);
-  const totalPrice = calculateTotalPrice()
+  const totalPrice = calculateTotalPrice();
   console.log({ cart });
   return (
     <div>
-      {products.map((product) => (
-        <div key={product._id} className="p-6 flex flex-col">
-          <p>product: {product.name}</p>
-          <p>price: {product.price}</p>
-          <div>
-            <button onClick={() => increaseQuantity(product)}>Increse</button>
+      {products.map((product) => {
+        const x = cart.map(item => {
+          if(item.product.name === product.name) {
+            return item
+          }
+        })[0]
+        return (
+          <div key={product._id} className="border border-red-500">
+            <p className="">product: {product.name}</p>
+            <p>price: {product.price}</p>
+            <div className="flex gap-5">
+              <button onClick={() => increaseQuantity(product)}>Increse</button>
+              <p>{x ? x.quantity : "0"}</p>
+              <button onClick={() => decreaseQuantity(product)}>Decrese</button>
+            </div>
           </div>
-          <div>
-            <button onClick={() => decreaseQuantity(product)}>Decrese</button>
-          </div>
-        </div>
-      ))}
+        );
+      })}
       <p>Total Price: {totalPrice}</p>
       <pre>{JSON.stringify(cart, undefined, 2)}</pre>
     </div>
