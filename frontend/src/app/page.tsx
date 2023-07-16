4e81df2d795907da8f36ad9252e4b273f43c3794
Kamal Singh <kamalsingh200238@gmail.com>
2023-07-16 11:58:17 +0530

    fix(frontend): remove styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   const dataString = JSON.stringify(data, undefined, 2);
   console.log({ data });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       {/* <pre>{dataString}</pre> */}
       <Temp products={data} />
     </main>
