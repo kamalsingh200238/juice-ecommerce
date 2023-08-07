55604ba98eefa4c670eca561a9622396169c7f0c
Kamal Singh <kamalsingh200238@gmail.com>
2023-08-07 17:12:30 +0530

    feat(frontend): Use new derived state

--- frontend/src/components/temp.tsx
+++ frontend/src/components/temp.tsx
@@ -1,37 +1,34 @@
-"use client";
+'use client';
 
-import { useCartStore } from "@/lib/store";
-import calculateTotalPrice from "@/lib/total-price";
-import { Product } from "@/types";
+import { useCartStore } from '@/lib/store';
+import { Product } from '@/types';
 
 export default function Temp({ products }: { products: Product[] }) {
   const cart = useCartStore((state) => state.cart);
+  const totalPrice = useCartStore((state) => state.totalCartPrice);
+  const totalQuantity = useCartStore((state) => state.totalCartQuantity);
   const increaseQuantity = useCartStore((state) => state.increaseQuantity);
   const decreaseQuantity = useCartStore((state) => state.decreaseQuantity);
-  const totalPrice = calculateTotalPrice();
   console.log({ cart });
   return (
     <div>
       {products.map((product) => {
-        const x = cart.map(item => {
-          if(item.product.name === product.name) {
-            return item
-          }
-        })[0]
+        const inCart = cart.find((cartItem) => cartItem.product._id === product._id);
+        const quantity = inCart ? inCart.quantity : 0;
         return (
-          <div key={product._id} className="border border-red-500">
-            <p className="">product: {product.name}</p>
+          <div key={product._id}>
+            <p>product: {product.name}</p>
             <p>price: {product.price}</p>
-            <div className="flex gap-5">
+            <div>
               <button onClick={() => increaseQuantity(product)}>Increse</button>
-              <p>{x ? x.quantity : "0"}</p>
+              <p>{quantity}</p>
               <button onClick={() => decreaseQuantity(product)}>Decrese</button>
             </div>
           </div>
         );
       })}
       <p>Total Price: {totalPrice}</p>
-      <pre>{JSON.stringify(cart, undefined, 2)}</pre>
+      <p>Total Quantity: {totalQuantity}</p>
     </div>
   );
 }
